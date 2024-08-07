881c798beb1c58722b196cd53258999d921c851e
Unanam Victoria <riaahh20@yahoo.com>
2024-08-07 19:56:05 +0000

    corrections

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,15 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized_():
+    """Returns a 401 unauthorized error"""
+    abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden():
+    """Returns a 401 unauthorized error"""
+    abort(403)
